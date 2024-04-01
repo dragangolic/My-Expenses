aa6f5f7982d0c9e64b3db38ea9a0aae373e751db
Dragan Golic <golicdragan0@gmail.com>
2024-04-01 13:30:47 -0400

    SQL update

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QComboBox, QPushButton, QLineEdit, QDateEdit, QTableWidget, \
-    QVBoxLayout, QHBoxLayout
+    QVBoxLayout, QHBoxLayout, QMessageBox, QTableWidgetItem, QHeaderView
+from PyQt5.QtSql import QSqlDatabase, QSqlQuery
+from PyQt5.QtCore import QDate, Qt
+import sys
 
 
 # App Class
@@ -12,10 +15,10 @@ class ExpenseApp(QWidget):
         self.setWindowTitle("My-Expenses")
 
         self.date_box = QDateEdit()
+        self.date_box.setDate(QDate.currentDate())
         self.dropdown = QComboBox()
         self.amount = QLineEdit()
         self.description = QLineEdit()
-
 
         self.add_button = QPushButton("Add Expense")
         self.delete_button = QPushButton("Delete Expense")
@@ -23,8 +26,47 @@ class ExpenseApp(QWidget):
         self.table = QTableWidget()
         self.table.setColumnCount(5)  # Id, date, category, amount, description
         self.table.setHorizontalHeaderLabels(['Id', 'Date', 'Category', 'Amount', 'Description'])
+        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.table.sortByColumn(1, Qt.DescendingOrder)
 
         # Design App with Layouts
+
+        self.dropdown.addItems(["Food", "Transportation", "Rent", "Shopping", "Entertainment", "Bills", "Other"])
+
+        self.setStyleSheet("""
+                            QWidget {
+                                    background-color: #b8c9e1;
+                                    }
+                            QLabel {
+                                    color: #333
+                                    font-size: 14px;
+                                    }
+                                    
+                            QlineEdit, QComboBox, QDateEdit, QPushButton{
+                                    background-color: #b8c9e1;
+                                    color: #333;
+                                    border: 1px solid #444;
+                                    padding: 5px;
+                                    }
+                                    
+                            QTableWidget{
+                                    background-color: #b8c9e1;
+                                    color: #333;
+                                    border: 1px solid #444;
+                                    selection-background-color: #ddd;    
+                                    }
+                            QPushButton{
+                                    background-color: #4caf50;
+                                    color: #fff;
+                                    border: none;
+                                    padding: 8px 16px;
+                                    font-size:14px;
+                                    }
+                            QPushButton:hover{
+                                    background-color: #45a049;
+                                    }
+                            """)
+
         self.master_layout = QVBoxLayout()
         self.row1 = QHBoxLayout()
         self.row2 = QHBoxLayout()
@@ -42,6 +84,8 @@ class ExpenseApp(QWidget):
 
         self.row3.addWidget(self.add_button)
         self.row3.addWidget(self.delete_button)
+        self.add_button.clicked.connect(self.add_expense)
+        self.delete_button.clicked.connect(self.delete_expense)
 
         self.master_layout.addLayout(self.row1)
         self.master_layout.addLayout(self.row2)
@@ -51,9 +95,98 @@ class ExpenseApp(QWidget):
 
         self.setLayout(self.master_layout)
 
+        self.load_table()
+
+    def load_table(self):
+        self.table.setRowCount(0)
+
+        query = QSqlQuery("SELECT * FROM expenses")
+        row = 0
+        while query.next():
+            expense_id = query.value(0)
+            date = query.value(1)
+            category = query.value(2)
+            amount = query.value(3)
+            description = query.value(4)
+
+            # Add values to Table
+            self.table.insertRow(row)
+
+            self.table.setItem(row, 0, QTableWidgetItem(str(expense_id)))
+            self.table.setItem(row, 1, QTableWidgetItem(date))
+            self.table.setItem(row, 2, QTableWidgetItem(category))
+            self.table.setItem(row, 3, QTableWidgetItem(str(amount)))
+            self.table.setItem(row, 4, QTableWidgetItem(description))
+
+            row += 1
+
+    def add_expense(self):
+        date = self.date_box.date().toString("yyyy-MM-dd")
+        category = self.dropdown.currentText()
+        amount = self.amount.text()
+        description = self.description.text()
+
+        query = QSqlQuery()
+        query.prepare("""
+                        INSERT INTO expenses(date, category, amount, description)
+                        VALUES(?, ?, ?, ?)
+                        """)
+        # Add binding value
+        query.addBindValue(date)
+        query.addBindValue(category)
+        query.addBindValue(amount)
+        query.addBindValue(description)
+        query.exec_()
+
+        # Prepare for the next input
+        self.date_box.setDate(QDate.currentDate())
+        self.dropdown.setCurrentIndex(0)
+        self.amount.clear()
+        self.description.clear()
+
+        self.load_table()
+
+    def delete_expense(self):
+        selected_row = self.table.currentRow()
+        if selected_row == -1:
+            QMessageBox.warning(self, "No expense Chosen", "Please choose an expense to delete!")
+            return
+
+        expense_id = int(self.table.item(selected_row, 0).text())
+        confirm = QMessageBox.question(self, "Are you sure?", "Delete Expense?", QMessageBox.Yes | QMessageBox.No)
+
+        if confirm == QMessageBox.No:
+            return
+
+        query = QSqlQuery()
+        query.prepare("Delete from expenses where id = ?")
+        query.addBindValue(expense_id)
+        query.exec_()
+
+        self.load_table()
+
+# Create database
+
+
+database = QSqlDatabase.addDatabase("QSQLITE")
+database.setDatabaseName("expense.db")
+if not database.open():
+    QMessageBox.critical(None, "Error", "Could not open your Database")
+    sys.exit()
+
+# Initial query
+query = QSqlQuery()
+query.exec_("""
+        CREATE TABLE IF NOT EXISTS expenses(
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Date TEXT,
+            Category TEXT,
+            Amount REAL,
+            Description TEXT
+        )
+        """)
+
 # Run the App
-
-
 if __name__ in "__main__":
     app = QApplication([])
     main = ExpenseApp()
